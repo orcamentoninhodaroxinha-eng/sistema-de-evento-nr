d4461e908d5331cdcd497a517a43d0644c2fc564
NINHO DA ROXINHA EVENTOS <orcamentoninhodaroxinha@gmail.com>
2026-03-31 22:18:30 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import LoginGate from './components/LoginGate';
 import Layout from './components/Layout';
 import Events from './pages/Events';
 import EventDetailPage from './pages/EventDetailPage';
@@ -12,19 +13,21 @@ import EventFormPage from './pages/EventFormPage';
 function App() {
 
   return (
-    <QueryClientProvider client={queryClientInstance}>
-      <Router>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Events />} />
-            <Route path="/events/new" element={<EventFormPage />} />
-            <Route path="/events/:id" element={<EventDetailPage />} />
-            <Route path="/events/:id/edit" element={<EventFormPage />} />
-          </Route>
-        </Routes>
-      </Router>
-      <Toaster />
-    </QueryClientProvider>
+    <LoginGate>
+      <QueryClientProvider client={queryClientInstance}>
+        <Router>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Events />} />
+              <Route path="/events/new" element={<EventFormPage />} />
+              <Route path="/events/:id" element={<EventDetailPage />} />
+              <Route path="/events/:id/edit" element={<EventFormPage />} />
+            </Route>
+          </Routes>
+        </Router>
+        <Toaster />
+      </QueryClientProvider>
+    </LoginGate>
   )
 }
 
