887e26d1a39ed48c30d705067cb1afd8a6cb3992
NINHO DA ROXINHA EVENTOS <orcamentoninhodaroxinha@gmail.com>
2026-03-31 23:28:43 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import LoginGate from './components/LoginGate';
 import Layout from './components/Layout';
 import Events from './pages/Events';
 import AccountDeletion from './pages/AccountDeletion';
@@ -39,14 +40,16 @@ const AuthenticatedApp = () => {
 
 function App() {
   return (
-    <AuthProvider>
-      <QueryClientProvider client={queryClientInstance}>
-        <Router>
-          <AuthenticatedApp />
-        </Router>
-        <Toaster />
-      </QueryClientProvider>
-    </AuthProvider>
+    <LoginGate>
+      <AuthProvider>
+        <QueryClientProvider client={queryClientInstance}>
+          <Router>
+            <AuthenticatedApp />
+          </Router>
+          <Toaster />
+        </QueryClientProvider>
+      </AuthProvider>
+    </LoginGate>
   )
 }
 
