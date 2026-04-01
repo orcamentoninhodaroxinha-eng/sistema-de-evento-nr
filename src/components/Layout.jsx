64746d6a6a646c2a4a523cccae6e93dab1b940c8
NINHO DA ROXINHA EVENTOS <orcamentoninhodaroxinha@gmail.com>
2026-04-01 21:15:38 +0000

    File changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -37,8 +37,7 @@ export default function Layout() {
     const touchEnd = e.changedTouches[0].clientY;
     const diff = touchStart - touchEnd;
     if (Math.abs(diff) > 30) {
-      const scroll = diff > 0 ? 1800 : -1800;
-      smoothScroll(scroll);
+      smoothScroll(diff * 2);
     }
     setTouchStart(null);
   };
