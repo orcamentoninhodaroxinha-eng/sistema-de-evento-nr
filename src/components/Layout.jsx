e74414abb2ef43343ad18be7c8355a990ea8446a
NINHO DA ROXINHA EVENTOS <orcamentoninhodaroxinha@gmail.com>
2026-04-01 02:47:36 +0000

    File changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -37,8 +37,8 @@ export default function Layout() {
 
       <main
         ref={mainRef}
-        className="flex-1 w-full px-3 sm:px-6 lg:px-8 py-3 sm:py-6 overflow-y-auto overflow-x-hidden [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-border [&::-webkit-scrollbar-thumb]:rounded-full"
-        style={{ WebkitOverflowScrolling: 'touch' }}
+        className="flex-1 w-full px-4 sm:px-6 lg:px-8 py-4 sm:py-6 overflow-y-auto overflow-x-hidden [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-border [&::-webkit-scrollbar-thumb]:rounded-full"
+        style={{ WebkitOverflowScrolling: 'touch', scrollbarWidth: 'thin', scrollbarColor: 'hsl(var(--border)) transparent' }}
       >
         <div className="max-w-7xl mx-auto">
           <Outlet />
