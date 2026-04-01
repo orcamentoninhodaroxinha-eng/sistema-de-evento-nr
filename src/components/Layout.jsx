f8435b955304639fb80bbbfeb2443681bf1a7976
NINHO DA ROXINHA EVENTOS <orcamentoninhodaroxinha@gmail.com>
2026-04-01 00:45:36 +0000

    File changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -25,7 +25,7 @@ export default function Layout() {
         </header>
 
       {/* Main content */}
-      <main className="flex-1 w-full px-4 sm:px-6 lg:px-8 py-4 sm:py-6 pb-8 overflow-x-hidden overflow-y-auto" style={{ WebkitOverflowScrolling: 'touch' }}>
+      <main className="flex-1 w-full px-4 sm:px-6 lg:px-8 py-4 sm:py-6 pb-8 overflow-x-hidden" style={{ overflowY: 'scroll', WebkitOverflowScrolling: 'touch' }} style={{ WebkitOverflowScrolling: 'touch' }}>
         <div className="max-w-7xl mx-auto">
           <Outlet />
         </div>
